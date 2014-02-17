5a55841e0d6a26d177b9d698481580d18e42be2c
dongfangx <dongfang.x@qq.com>
2014-02-17 19:46:55 +0800

    edit upload

--- src/com/ly/controller/ImgController.java
+++ src/com/ly/controller/ImgController.java
@@ -1,15 +1,22 @@
 package com.ly.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.jfinal.core.Controller;
 import com.jfinal.kit.PathKit;
 import com.jfinal.plugin.ehcache.CacheKit;
 import com.jfinal.upload.UploadFile;
 import com.ly.model.Img;
 import com.ly.tool.Dwz;
+import com.ly.vo.FileUploadInfo;
+import org.nutz.lang.Files;
+import org.nutz.lang.random.StringGenerator;
 
 import javax.servlet.http.HttpSession;
+import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 
@@ -51,8 +58,30 @@ public class ImgController extends Controller {
         String real_path = upFile.getSaveDirectory();
         String  pathAndName = real_path + fileName;
           */
+        List<FileUploadInfo> fileInfos = new LinkedList<FileUploadInfo>();
         List<UploadFile> files = getFiles(pathUrl, maxSize, "utf-8");
-        System.out.println(" ---- " + files.size());
+        for (UploadFile uploadFile : files)
+        {
+            FileUploadInfo fileInfo = new FileUploadInfo();
+
+            File f = uploadFile.getFile();
+            String type = Files.getSuffixName(f);
+
+            StringGenerator sg = new StringGenerator(3);
+            String fileName = sg.next() + System.currentTimeMillis() + "." + type;
+            Files.rename(f, fileName);
+
+            fileInfo.setName(fileName);
+            fileInfo.setUrl("/upload/"+fileName);
+            fileInfo.setThumbnailUrl("/upload/"+fileName);
+            fileInfo.setDeleteType("DELETE");
+            fileInfo.setDeleteUrl("");
+            fileInfo.setSize(f.getFreeSpace());
+            fileInfos.add(fileInfo);
+        }
+
+
+        System.out.println(" ---- " + files.size() + "        " + JSON.toJSONString(fileInfos));
 
         HttpSession session = getSession();
         Object o = session.getAttribute("userid");
@@ -62,10 +91,16 @@ public class ImgController extends Controller {
         img.set("userid",o);
         img.set("adddate",new Date());
         boolean ok = Img.imgDao.saveOrUpdate(img);
-        if (ok)
-        {
-            redirect("/upload");
-        }
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("{\"files\":");
+        sb.append(JSON.toJSONString(fileInfos));
+        sb.append("}");
+
+        System.out.println(sb.toString());
+
+        renderJson(sb.toString());
+
     }
 
     public void del()
---
+++ src/com/ly/vo/FileUploadInfo.java
@@ -0,0 +1,77 @@
+package com.ly.vo;
+
+/**
+ * Created by zhongwei on 2/17/14.
+ */
+public class FileUploadInfo {
+
+    private String url;
+
+    private String thumbnailUrl;
+
+    private String name;
+
+    private String type;
+
+    private Long size;
+
+    private String deleteUrl;
+
+    private String deleteType;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public String getDeleteUrl() {
+        return deleteUrl;
+    }
+
+    public void setDeleteUrl(String deleteUrl) {
+        this.deleteUrl = deleteUrl;
+    }
+
+    public String getDeleteType() {
+        return deleteType;
+    }
+
+    public void setDeleteType(String deleteType) {
+        this.deleteType = deleteType;
+    }
+}
